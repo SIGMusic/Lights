dd0c289cc658e5ca9f9afeac9bdfbc3c282e1a40
zricethezav <zachrice9@gmail.com>
2014-02-02 15:31:25 -0600

    fun lights

--- Lights.ino
+++ Lights.ino
@@ -33,3 +33,31 @@ void loop()
     strip.show();
     delay(10);
 }
+
+/*
+  Helper function for RGB strip library
+*/
+
+uint32_t Wheel(uint16_t WheelPos)
+{
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128;   //Red down
+      g = WheelPos % 128;      // Green up
+      b = 0;                  //blue off
+      break; 
+    case 1:
+      g = 127 - WheelPos % 128;  //green down
+      b = WheelPos % 128;      //blue up
+      r = 0;                  //red off
+      break; 
+    case 2:
+      b = 127 - WheelPos % 128;  //blue down 
+      r = WheelPos % 128;      //red up
+      g = 0;                  //green off
+      break; 
+  }
+  return(strip.Color(r,g,b));
+}
